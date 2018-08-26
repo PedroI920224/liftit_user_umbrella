a01574475a1714172f02194702d75886748fa0c8
PedroI920224 <pedro92_0224@hotmail.com>
2018-08-26 14:13:22 -0500

    add password_hash and unique constrain in email

--- apps/liftit_user_db/priv/repo/migrations/20180819043945_create_user.exs
+++ apps/liftit_user_db/priv/repo/migrations/20180819043945_create_user.exs
@@ -5,7 +5,7 @@ defmodule LiftitUserDb.Repo.Migrations.CreateUser do
     create table(:users) do
       add :name, :string, null: false
       add :email, :string, null: false
-      add :password, :string, null: false
+      add :password_hash, :string, null: false
       add :confirmed, :boolean, default: false, null: false
       add :phone_number, :string, null: false
       add :country, :string
@@ -15,5 +15,6 @@ defmodule LiftitUserDb.Repo.Migrations.CreateUser do
       timestamps()
     end
 
+    create unique_index(:users, [:email])
   end
 end
